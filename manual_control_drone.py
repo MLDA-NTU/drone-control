73823e57fc2bdbc8c94e7ccfd8238e12afad4e36
andrewivan123 <andrew.ivan@ymail.com>
2020-08-19 19:44:07 +0800

    First commit

---
+++ manual_control_drone.py
@@ -0,0 +1,49 @@
+from djitellopy import Tello
+import cv2
+import time
+from threading import Thread
+
+tello = Tello()
+tello.connect()
+tello.takeoff()
+tello.streamon()
+
+controller = True
+key = None
+
+
+def controls():
+    while(controller):
+        if key == ord('w'):
+            tello.move_forward(30)
+        elif key == ord('s'):
+            tello.move_back(30)
+        elif key == ord('a'):
+            tello.move_left(30)
+        elif key == ord('d'):
+            tello.move_right(30)
+        elif key == ord('e'):
+            tello.rotate_clockwise(30)
+        elif key == ord('q'):
+            tello.rotate_counter_clockwise(30)
+        elif key == ord('r'):
+            tello.move_up(30)
+        elif key == ord('f'):
+            tello.move_down(30)
+
+Thread(target=controls, daemon=True).start()
+frame_read = tello.get_frame_read()
+while(True):
+    img = frame_read.frame
+    if img is None:
+        continue
+    # Display the resulting frame
+    cv2.imshow('frame', img)
+    key = cv2.waitKey(1) & 0xff
+    if key == 27: 
+        tello.land()
+        cv2.destroyAllWindows()# ESC
+        tello.streamoff()
+        controller = False
+        break
+exit()
